38597d2dceef042cbbaef7351a402c29c1ccabec
Talen Fisher <talen.fisher@cythral.com>
2021-07-20 21:18:41 -0500

    Update OutputData.cs

--- src/Resources.Command/OutputData.cs
+++ src/Resources.Command/OutputData.cs
@@ -7,11 +7,11 @@ namespace Brighid.Commands.Resources
     /// <summary>
     /// Represents data returned to CloudFormation about a command.
     /// </summary>
-    public class OutputData : Command, ICustomResourceOutputData
+    public class OutputData : ICustomResourceOutputData
     {
         /// <summary>
-        /// Gets the command's id/name in string form.
+        /// Gets or sets the command's id/name in string form.
         /// </summary>
-        public new string Id => Name;
+        public string Id { get; set; }
     }
 }

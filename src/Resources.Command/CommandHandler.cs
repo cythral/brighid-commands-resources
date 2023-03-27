4062a7e2652d5cc52e09f88709675098780108d5
Talen Fisher <talen.fisher@cythral.com>
2023-03-27 11:52:04 -0500

    Add Logging

--- src/Resources.Command/CommandHandler.cs
+++ src/Resources.Command/CommandHandler.cs
@@ -1,3 +1,4 @@
+using System.Text.Json;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -5,6 +6,8 @@ using Brighid.Commands.Client;
 
 using Lambdajection.Attributes;
 using Lambdajection.CustomResource;
+
+using Microsoft.Extensions.Logging;
 
 namespace Brighid.Commands.Resources
 {
@@ -16,16 +19,20 @@ namespace Brighid.Commands.Resources
     {
         private readonly ICommandsClient commandsClient;
         private readonly ClientRequestOptions requestOptions = default;
+        private readonly ILogger<CommandHandler> logger;
 
         /// <summary>
         /// Initializes a new instance of the <see cref="CommandHandler" /> class.
         /// </summary>
         /// <param name="commandsClient">Client to use for interacting with the commands service.</param>
+        /// <param name="logger">Logger used to log information to stdout.</param>
         public CommandHandler(
-            ICommandsClient commandsClient
+            ICommandsClient commandsClient,
+            ILogger<CommandHandler> logger
         )
         {
             this.commandsClient = commandsClient;
+            this.logger = logger;
         }
 
 #pragma warning disable IDE0060
@@ -38,6 +45,7 @@ namespace Brighid.Commands.Resources
         /// <returns>The resulting command data.</returns>
         public async Task<OutputData> Create(CustomResourceRequest<CommandRequest> request, CancellationToken cancellationToken = default)
         {
+            logger.LogInformation("Received create request: {@request}", JsonSerializer.Serialize(request));
             var result = await commandsClient.CreateCommand(request.ResourceProperties, requestOptions, cancellationToken);
             return new OutputData { Id = result.Name };
         }
@@ -50,6 +58,7 @@ namespace Brighid.Commands.Resources
         /// <returns>The resulting command data.</returns>
         public async Task<OutputData> Update(CustomResourceRequest<CommandRequest> request, CancellationToken cancellationToken = default)
         {
+            logger.LogInformation("Received update request: {@request}", JsonSerializer.Serialize(request));
             var result = await commandsClient.UpdateCommand(request.PhysicalResourceId, request.ResourceProperties, requestOptions, cancellationToken);
             return new OutputData { Id = result.Name };
         }
@@ -62,6 +71,7 @@ namespace Brighid.Commands.Resources
         /// <returns>The resulting command data.</returns>
         public async Task<OutputData> Delete(CustomResourceRequest<CommandRequest> request, CancellationToken cancellationToken = default)
         {
+            logger.LogInformation("Received delete request: {@request}", JsonSerializer.Serialize(request));
             var result = await commandsClient.DeleteCommand(request.PhysicalResourceId, requestOptions, cancellationToken);
             return new OutputData { Id = result.Name };
         }
